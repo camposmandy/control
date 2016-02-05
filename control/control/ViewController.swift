cc516a9139657349b7d4c27ac1623ead216b7b55
Amanda Campos <Amanda@amanda.local>
2016-02-05 14:32:19 -0200

    Design

--- control/control/ViewController.swift
+++ control/control/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UIButton!
+    
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.layer.cornerRadius = 75
+        label.layer.borderColor = UIColor.whiteColor().CGColor
+        label.layer.backgroundColor = UIColor(red: 27/255, green: 188/255, blue: 155/255, alpha: 0.8).CGColor
+        
+        label.layer.shadowColor = UIColor.blackColor().CGColor
+        label.layer.shadowOffset = CGSizeZero
+        label.layer.shadowOpacity = 3
+        label.layer.masksToBounds = false
+            
+        button.layer.cornerRadius = 75
+        button.layer.borderColor = UIColor.whiteColor().CGColor
+        button.layer.backgroundColor = UIColor(red: 27/255, green: 188/255, blue: 155/255, alpha: 0.8).CGColor
+        
+        button.layer.shadowColor = UIColor.blackColor().CGColor
+        button.layer.shadowOffset = CGSizeZero
+        button.layer.shadowOpacity = 3
+        button.layer.masksToBounds = false
     }
 
     override func didReceiveMemoryWarning() {
