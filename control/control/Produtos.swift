51397829fa694536705c72d402442cf569818c52
mmedeiro <31313132@mackezista.com.br>
2016-03-15 09:14:38 -0300

    Manager da entidade "Produtos"

--- control/control/Produtos.swift
+++ control/control/Produtos.swift
@@ -8,10 +8,51 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 
 class Produtos: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass
+    
+    static let sharedInstance = Produtos()
+    static let entityName:String = "Produtos"
+    
+    let coreData = CoreDataManager.sharedInstance
+    
+   // private init(){}
+    
+    
+    func novoProduto() -> control.Produtos {
+        return NSEntityDescription.insertNewObjectForEntityForName(Produtos.entityName, inManagedObjectContext: coreData.managedObjectContext!) as! control.Produtos
+    }
+    
+    
+    func Produto() -> Array<control.Produtos>? {
+        return coreData.fetchData(Produtos.entityName, predicate: NSPredicate(format: "TRUEPREDICATE")) as? Array<control.Produtos>
+    }
+    
+    
+    func Produto(predicate: NSPredicate) -> Array<control.Produtos>? {
+        return coreData.fetchData(Produtos.entityName, predicate: predicate) as? Array<control.Produtos>
+    }
+    
+    
+    func salvar(){
+        coreData.saveContext()
+    }
+    
+    
+    func deletar(Produtos: control.Produtos) {
+        coreData.managedObjectContext?.deleteObject(Produtos)
+    }
+    
+    
+    func deletarTudo(){
+        let allObjects: Array<control.Produtos> = self.Produto()!
+        for item: control.Produtos in allObjects as Array<control.Produtos> {
+            self.deletar(item)
+        }
+    }
 
 }
